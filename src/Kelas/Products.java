1a89e3f93023f2e98c68af62f4f138dcab7b2fe1
rizalzaul28 <rizandi778@gmail.com>
2024-10-20 01:41:28 +0700

    Latihan PBO Part 1

---
+++ src/Kelas/Products.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Kelas;
+
+/**
+ *
+ * @author MysticWave
+ */
+public class Products {
+
+    int produk_id, produk_price, produk_card_id;
+    String produk_name, produk_desc;        
+
+    public int getProduk_id() {
+        return produk_id;
+    }
+
+    public void setProduk_id(int produk_id) {
+        this.produk_id = produk_id;
+    }
+
+    public int getProduk_price() {
+        return produk_price;
+    }
+
+    public void setProduk_price(int produk_price) {
+        this.produk_price = produk_price;
+    }
+
+    public int getProduk_card_id() {
+        return produk_card_id;
+    }
+
+    public void setProduk_card_id(int produk_card_id) {
+        this.produk_card_id = produk_card_id;
+    }
+
+    public String getProduk_name() {
+        return produk_name;
+    }
+
+    public void setProduk_name(String produk_name) {
+        this.produk_name = produk_name;
+    }
+
+    public String getProduk_desc() {
+        return produk_desc;
+    }
+
+    public void setProduk_desc(String produk_desc) {
+        this.produk_desc = produk_desc;
+    }
+
+    
+}
